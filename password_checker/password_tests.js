5e8fa6352761d4ae6de15f82a5bea8bda8848363
Manelisi Mpotulo <mpotulom28@gmail.com>
2024-03-20 14:24:34 +0200

    Create password_tests.js

---
+++ password_checker/password_tests.js
@@ -0,0 +1,23 @@
+
+
+// const testCases = [
+//     { password: "^&%$#@(&^$#%", expected: "weak" },
+//     { password: " 877564", expected: "medium" }, //invalid
+//     { password: "nasmeeeeeIS9999$$", expected: "strong" },
+//     { password: "APASSWORD", expected: "weak" },
+//     { password: " ", expected: "weak" }, //invalid
+//     { password: "4567842355", expected: "weak" },
+//     { password: " IS9999word", expected: "strong" },
+//     { password: "$%*& ", expected: "medium" }, //invalid
+//     { password: "My999@99 nasmeeeee", expected: "strong" },
+// ];
+
+// testCases.forEach( ( { password, expected } ) => {
+//     const result = passwordStrength( password );
+
+//     if ( result === expected ) {
+//         console.log( `\nTEST PASSED! password: |${ password }| \nExpected |${ expected }|, and got |${ result }|` );
+//     } else {
+//         console.log( `\nTest failed! password: |${ password }| \nExpected |${ expected }|, but got |${ result }|` );
+//     }
+// } );
